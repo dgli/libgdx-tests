2ae917ccc882c2623d3989b713ce6fab4c582b2d
dgli <facultyofmusic@gmail.com>
2015-01-01 00:32:38 -0500

    Added some GUI components to test with.  Going to make a menu to navigate to different test areas.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,221 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	int cc = 0;
-	
+public class MyGdxGame extends InputAdapter implements ApplicationListener {
+	Object[] listEntries = {"This is a list entry1", "And another one1", "The meaning of life1", "Is hard to come by1",
+			"This is a list entry2", "And another one2", "The meaning of life2", "Is hard to come by2", "This is a list entry3",
+			"And another one3", "The meaning of life3", "Is hard to come by3", "This is a list entry4", "And another one4",
+			"The meaning of life4", "Is hard to come by4", "This is a list entry5", "And another one5", "The meaning of life5",
+			"Is hard to come by5"};
+
+	Skin skin;
+	Stage stage;
+	Texture texture1;
+	Texture texture2;
+	Label fpsLabel;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+		texture1 = new Texture(Gdx.files.internal("badlogicsmall.jpg"));
+		texture2 = new Texture(Gdx.files.internal("badlogic.jpg"));
+		TextureRegion image = new TextureRegion(texture1);
+		TextureRegion imageFlipped = new TextureRegion(image);
+		imageFlipped.flip(true, true);
+		TextureRegion image2 = new TextureRegion(texture2);
+		// stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, new PolygonSpriteBatch());
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+
+		// Group.debug = true;
+
+		ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle(skin.get(Button.ButtonStyle.class));
+		style.imageUp = new TextureRegionDrawable(image);
+		style.imageDown = new TextureRegionDrawable(imageFlipped);
+		ImageButton iconButton = new ImageButton(style);
+
+		Button buttonMulti = new TextButton("Multi\nLine\nToggle", skin, "toggle");
+		Button imgButton = new Button(new Image(image), skin);
+		Button imgToggleButton = new Button(new Image(image), skin, "toggle");
+
+		Label myLabel = new Label("this is some text.", skin);
+		myLabel.setWrap(true);
+
+		// my own table
+		Table table = new Table();
+
+		table.row();
+		table.add(imgButton).expandX();
+		table.add(myLabel).pad(10);
+
+		table.row();
+		table.add(imgToggleButton).width(200);
+		table.add(iconButton).expandX();
+
+		table.setFillParent(true);
+		stage.addActor(table);
+
+//		Table t = new Table();
+//		t.row();
+//		t.add(myLabel);
+//
+//		t.layout();
+
+//		final CheckBox checkBox = new CheckBox(" Continuous rendering", skin);
+//		checkBox.setChecked(true);
+//		final Slider slider = new Slider(0, 10, 1, false, skin);
+//		slider.setAnimateDuration(0.3f);
+//		TextField textfield = new TextField("", skin);
+//		textfield.setMessageText("Click here!");
+//		final SelectBox dropdown = new SelectBox(skin);
+//		dropdown.addListener(new ChangeListener() {
+//			public void changed (ChangeEvent event, Actor actor) {
+//				System.out.println(dropdown.getSelected());
+//			}
+//		});
+//		dropdown.setItems("Android1", "Windows1", "Linux1", "OSX1", "Android2", "Windows2", "Linux2", "OSX2", "Android3",
+//				"Windows3", "Linux3", "OSX3", "Android4", "Windows4", "Linux4", "OSX4", "Android5", "Windows5", "Linux5", "OSX5",
+//				"Android6", "Windows6", "Linux6", "OSX6", "Android7", "Windows7", "Linux7", "OSX7");
+//		dropdown.setSelected("Linux6");
+//		Image imageActor = new Image(image2);
+//		ScrollPane scrollPane = new ScrollPane(imageActor);
+//		List list = new List(skin);
+//		list.setItems(listEntries);
+//		list.getSelection().setMultiple(true);
+//		list.getSelection().setRequired(false);
+//		// list.getSelection().setToggle(true);
+//		ScrollPane scrollPane2 = new ScrollPane(list, skin);
+//		scrollPane2.setFlickScroll(false);
+//		SplitPane splitPane = new SplitPane(scrollPane, scrollPane2, false, skin, "default-horizontal");
+//		fpsLabel = new Label("fps:", skin);
+//
+//		// configures an example of a TextField in password mode.
+//		final Label passwordLabel = new Label("Textfield in password mode: ", skin);
+//		final TextField passwordTextField = new TextField("", skin);
+//		passwordTextField.setMessageText("password");
+//		passwordTextField.setPasswordCharacter('*');
+//		passwordTextField.setPasswordMode(true);
+//
+//		// window.debug();
+//		Window window = new Window("Dialog", skin);
+//		window.getButtonTable().add(new TextButton("X", skin)).height(window.getPadTop());
+//		window.setPosition(0, 0);
+//		window.defaults().spaceBottom(10);
+//		window.row().fill().expandX();
+//		window.add(iconButton);
+//		window.add(buttonMulti);
+//		window.add(imgButton);
+//		window.add(imgToggleButton);
+//		window.row();
+//		window.add(checkBox);
+//		window.add(slider).minWidth(100).fillX().colspan(3);
+//		window.row();
+//		window.add(dropdown);
+//		window.add(textfield).minWidth(100).expandX().fillX().colspan(3);
+//		window.row();
+//		window.add(splitPane).fill().expand().colspan(4).maxHeight(200);
+//		window.row();
+//		window.add(passwordLabel).colspan(2);
+//		window.add(passwordTextField).minWidth(100).expandX().fillX().colspan(2);
+//		window.row();
+//		window.add(fpsLabel).colspan(4);
+//		window.pack();
+//
+//		// stage.addActor(new Button("Behind Window", skin));
+//		stage.addActor(window);
+//
+//		textfield.setTextFieldListener(new TextField.TextFieldListener() {
+//			public void keyTyped (TextField textField, char key) {
+//				if (key == '\n') textField.getOnscreenKeyboard().show(false);
+//			}
+//		});
+//
+//		slider.addListener(new ChangeListener() {
+//			public void changed (ChangeEvent event, Actor actor) {
+//				Gdx.app.log("UITest", "slider: " + slider.getValue());
+//			}
+//		});
+//
+//		iconButton.addListener(new ChangeListener() {
+//			public void changed (ChangeEvent event, Actor actor) {
+//
+//				new Dialog("Some Dialog", skin, "dialog") {
+//					protected void result (Object object) {
+//						System.out.println("Chosen: " + object);
+//					}
+//				}.text("Are you enjoying this demo?").button("Yes", true).button("No", false).key(Input.Keys.ENTER, true)
+//						.key(Input.Keys.ESCAPE, false).show(stage);
+//
+//				Gdx.app.log("MyTag", "my informative message");
+//				Gdx.app.error("MyTag", "my error message");
+//				Gdx.app.debug("MyTag", "my debug message");
+//			}
+//		});
+//
+//		checkBox.addListener(new ChangeListener() {
+//			public void changed (ChangeEvent event, Actor actor) {
+//				Gdx.graphics.setContinuousRendering(checkBox.isChecked());
+//			}
+//		});
+
+
+		// stage debug
+		stage.setDebugAll(true);
+		//stage.setDebugTableUnderMouse(true);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
 
-		cc++;
+		//fpsLabel.setText("fps: " + Gdx.graphics.getFramesPerSecond());
 
-		batch.draw(img, cc, 100);
-		batch.end();
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
 	}
+
+	@Override
+	public void resize (int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void dispose () {
+		stage.dispose();
+		skin.dispose();
+		texture1.dispose();
+		texture2.dispose();
+	}
+
+
+
+	public void resume () {
+	}
+
+
+	public void pause () {
+	}
+
+
 }
